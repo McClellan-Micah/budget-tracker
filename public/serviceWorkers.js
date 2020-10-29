b842953a8ac8a2b579e2e2ffb25be624d380eaa6
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-10-29 09:21:23 -0700

    set to delete chaches

--- public/serviceWorkers.js
+++ public/serviceWorkers.js
@@ -64,3 +64,23 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+
+      cacheKeeplist.push(CACHE_NAME);
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
