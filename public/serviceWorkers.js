7024e00cd88a9cbbb7131d1bdb63124206797e93
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-10-29 09:10:36 -0700

    cache resourse/responses

--- public/serviceWorkers.js
+++ public/serviceWorkers.js
@@ -18,3 +18,49 @@ const FILES_TO_CACHE = [
   './icons/icon-384x384.png',
   './icons/icon-512x512.png',
 ];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
+        }
+      });
+    })
+  );
+});
